40a14effa1b3a01a16f477db7910fd2b469cdb27
Elchin Valiyev <valiyev.elchin@gmail.com>
2017-05-21 22:52:57 +0200

    Updated for TensorFlow 1.1

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ![](https://cloud.githubusercontent.com/assets/80381/20867532/97e01b94-ba8a-11e6-93f9-0c8fe0b76e00.png)
 
-https://www.tensorflow.org/versions/r0.12/how_tos/embedding_viz/index.html
+https://www.tensorflow.org/get_started/embedding_viz
 
 Use [imagenet model](https://www.tensorflow.org/versions/master/tutorials/image_recognition/index.html#usage-with-python-api) and visualize its outputs.
 
 ```
-$ pip install $TF_BINARY_URL<0.12.0rc0>
-$ python main.py
+$ pip3 install --upgrade tensorflow  
+$ python3 main.py
 $ tensorboard --logdir logdir
 ```
--- main.py
+++ main.py
@@ -2,19 +2,63 @@ import os
 import math
 import numpy as np
 import tensorflow as tf
-from tensorflow.models.image.imagenet import classify_image
+import download
 from tensorflow.contrib.tensorboard.plugins import projector
 
-classify_image.FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('model_dir', '/tmp/imagenet',
                            """Path to model.""")
 
+# Internet URL for the tar-file with the Inception model.
+# Note that this might change in the future and will need to be updated.
+data_url = "http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz"
+
+# File containing the TensorFlow graph definition. (Downloaded)
+path_graph_def = "classify_image_graph_def.pb"
+
+# Directory to store the downloaded data.
+data_dir = "inception/"
+
+
+def maybe_download():
+    """
+    Download the Inception model from the internet if it does not already
+    exist in the data_dir. The file is about 85 MB.
+    """
+
+    print("Downloading Inception v3 Model ...")
+    download.maybe_download_and_extract(url=data_url, download_dir=data_dir)
+
+
+def load_graph():
+    # Create a new TensorFlow computational graph.
+    graph = tf.Graph()
+
+    # Set the new graph as the default.
+    with graph.as_default():
+        # TensorFlow graphs are saved to disk as so-called Protocol Buffers
+        # aka. proto-bufs which is a file-format that works on multiple
+        # platforms. In this case it is saved as a binary file.
+
+        # Open the graph-def file for binary reading.
+        path = os.path.join(data_dir, path_graph_def)
+        with tf.gfile.FastGFile(path, 'rb') as file:
+            # The graph-def is a saved copy of a TensorFlow graph.
+            # First we need to create an empty graph-def.
+            graph_def = tf.GraphDef()
+
+            # Then we load the proto-buf file into the graph-def.
+            graph_def.ParseFromString(file.read())
+
+            # Finally we import the graph-def to the default TensorFlow graph.
+            tf.import_graph_def(graph_def, name='')
+    return graph
+
 
 def main(argv=None):
-    classify_image.maybe_download_and_extract()
-    classify_image.create_graph()
+    maybe_download()
+    graph = load_graph()
     basedir = os.path.dirname(__file__)
-    with tf.Session() as sess:
+    with tf.Session(graph=graph) as sess:
         pool3 = sess.graph.get_tensor_by_name('pool_3:0')
         jpeg_data = tf.placeholder(tf.string)
         thumbnail = tf.cast(tf.image.resize_images(tf.image.decode_jpeg(jpeg_data, channels=3), [100, 100]), tf.uint8)
@@ -32,18 +76,21 @@ def main(argv=None):
                 outputs.append(results[0])
                 images.append(results[1])
 
-        embedding_var = tf.Variable(tf.pack([tf.squeeze(x) for x in outputs], axis=0), trainable=False, name='pool3')
+        embedding_var = tf.Variable(tf.stack([tf.squeeze(x) for x in outputs], axis=0), trainable=False, name='embed')
+
         # prepare projector config
         config = projector.ProjectorConfig()
         embedding = config.embeddings.add()
         embedding.tensor_name = embedding_var.name
-        summary_writer = tf.train.SummaryWriter(os.path.join(basedir, 'logdir'))
+        summary_writer = tf.summary.FileWriter(os.path.join(basedir, 'logdir'))
+
         # link metadata
         metadata_path = os.path.join(basedir, 'logdir', 'metadata.tsv')
         with open(metadata_path, 'w') as f:
             for name in files:
                 f.write('%s\n' % name)
         embedding.metadata_path = metadata_path
+
         # write to sprite image file
         image_path = os.path.join(basedir, 'logdir', 'sprite.jpg')
         size = int(math.sqrt(len(images))) + 1
@@ -51,16 +98,19 @@ def main(argv=None):
             images.append(np.zeros((100, 100, 3), dtype=np.uint8))
         rows = []
         for i in range(size):
-            rows.append(tf.concat(1, images[i*size:(i+1)*size]))
-        jpeg = tf.image.encode_jpeg(tf.concat(0, rows))
+            rows.append(tf.concat(images[i * size:(i + 1) * size], 1))
+        jpeg = tf.image.encode_jpeg(tf.concat(rows, 0))
         with open(image_path, 'wb') as f:
             f.write(sess.run(jpeg))
+
         embedding.sprite.image_path = image_path
         embedding.sprite.single_image_dim.extend([100, 100])
+
         # save embedding_var
         projector.visualize_embeddings(summary_writer, config)
         sess.run(tf.variables_initializer([embedding_var]))
-        saver = tf.train.Saver([embedding_var])
+
+        saver = tf.train.Saver()
         saver.save(sess, os.path.join(basedir, 'logdir', 'model.ckpt'))
 
 
